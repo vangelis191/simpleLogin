efc1add28c1b24c8997ef45487bdc4222411f05f
Vangelis191 <vangelischasiotis@gmail.com>
2018-03-08 13:22:14 +0200

    Change to doPost

--- src/java/loginServlet/Login.java
+++ src/java/loginServlet/Login.java
@@ -14,7 +14,7 @@ import dao.LoginDaoImpl;
 public class Login extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
